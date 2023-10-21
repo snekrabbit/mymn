5cdc7abf09c327a68c3bbce1da4b09f05207c46c
Rob Giardina <rgiar@yahoo.com>
2023-10-20 20:36:41 -0400

    go up, down, n, s, ...

--- mymn.py
+++ mymn.py
@@ -18,13 +18,32 @@ def exit(room):
 
 def act(cmd):
     utils.trace ("you said " + cmd)
+
+    current_room = get_room()
+
     if cmd == "quit":
         lose()
-    elif cmd == "go up":
-        current_room = get_room()
-        next_room_name = current_room["directions"]["UP"]["room"]
-        next_room = rooms.rooms[next_room_name]
-        move(current_room, next_room)
+        return
+
+    if cmd == "go up":
+        dir = "UP"
+    elif cmd == "go down":
+        dir = "DOWN"
+    elif cmd == "go north":
+        dir = "N"
+    elif cmd == "go east":
+        dir = "E"
+    elif cmd == "go south":
+        dir = "S"
+    elif cmd == "go west":
+        dir = "W"
+    else:
+        utils.say("we can't move " + cmd + " in here")
+        return
+
+    next_room_name = current_room["directions"][dir]["room"]
+    next_room = rooms.rooms[next_room_name]
+    move(current_room, next_room)
 
 def move(from_room, to_room):
     exit(from_room)
@@ -34,11 +53,11 @@ def loop():
     utils.trace("looping inside room " + repr(rooms.current_room))
     cmd = get_room().get("automate")
     if cmd:
-        utils.type("> " + cmd) 
+        utils.type("> " + cmd)
     else:
         # no automate key, ask user
         cmd = input("> ")
-    
+
     act(cmd)
     loop()
 
